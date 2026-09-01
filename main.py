1d50ae8f23468f558a582911bd997b368660ea26
jupoeha <110650046+jupoeha@users.noreply.github.com>
2026-09-01 17:28:44 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import app_core
 if __name__ == '__main__':
-    app_core.PORT =	8000
+    #app_core.PORT=8000
     try:
         asyncio.run(app_core.main())
     except KeyboardInterrupt:
